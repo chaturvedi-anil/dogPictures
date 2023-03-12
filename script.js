e1ea4e6ff252de1c2a46487ed3572d436e5d28f6
chaturvedi-anil <chaturvedia560@gmail.com>
2023-03-12 21:42:51 +0530

    2nd way using jquery added

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
  
 function fetchRandomDogImages()
 {
-    var xhrRequest = new XMLHttpRequest();
+    
+    // <--------------Using ajax------------------>
 
-    xhrRequest.onload = function()
-    {
-        var responseJSON =JSON.parse(xhrRequest.response);
-        var imageUrl = responseJSON.message;
-        $('#dog-image').attr('src', imageUrl);
-    }
+    // var xhrRequest = new XMLHttpRequest();
+    // xhrRequest.onload = function()
+    // {
+    //     var responseJSON =JSON.parse(xhrRequest.response);
+    //     var imageUrl = responseJSON.message;
+    //     $('#dog-image').attr('src', imageUrl);
+    // }
 
-    xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random' , true);
-    xhrRequest.send();
+    // xhrRequest.open('get', 'https://dog.ceo/api/breeds/image/random' , true);
+    // xhrRequest.send();
+
+
+    // <-------------------Using Jquery-------------->
+
+    $.ajax(
+        {
+            url:'https://dog.ceo/api/breeds/image/random',
+            method:'GET',
+            success:function(data){
+                var imageUrl = data.message;
+                $('#dog-image').attr('src', imageUrl);
+            }
+        });
 }
 
 $('#fetch-dog-image-btn').click(fetchRandomDogImages);
